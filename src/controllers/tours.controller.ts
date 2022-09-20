ce89cc4c9b081a73fd1372ebe3ac6bd6dd70d00a
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-09-21 01:22:13 +0600

    tour post api

--- src/controllers/tours.controller.ts
+++ src/controllers/tours.controller.ts
@@ -1,8 +1,40 @@
 import { Request, Response } from "express";
+import { TourModel } from "../models/tours.model";
 
 // post data
 const createTour = async (req: Request, res: Response) => {
-  res.send("It's working! Yayyy");
+  const tour = new TourModel(req.body);
+  try {
+    await tour.save();
+    res.status(201).json({
+      status: "Successfully created tour data",
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+const GetAllTours = async (req: Request, res: Response) => {
+  try {
+    const tours = await TourModel.find();
+    res.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
-export const toursRouter = { createTour };
+export const toursRouter = { createTour, GetAllTours };
